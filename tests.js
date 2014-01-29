58831ef110d93bb3d3f554b478e1b659405c1804
Simon Menke <simon.menke@gmail.com>
2014-01-29 17:06:17 +0100

    Added better test

--- tests.js
+++ tests.js
@@ -2,12 +2,24 @@ QUnit.done(function(results){
   window.global_test_results = results;
 });
 
+function xhr() {
+  if (typeof window.XMLHttpRequest != "undefined"){
+      return new XMLHttpRequest();
+  }
+
+  if (typeof window.ActiveXObject != "undefined"){
+    try { return new ActiveXObject("Msxml2.XMLHTTP.4.0"); } catch(e) {}
+    try { return new ActiveXObject("MSXML2.XMLHTTP"); } catch(e) {}
+    try { return new ActiveXObject("Microsoft.XMLHTTP"); } catch(e) {}
+  }
+
+  return undefined;
+}
+
 test("Detected xhr.upload", function(){
-  var req = new XMLHttpRequest();
-  notEqual(req.upload, false, "OK!")
+  ok(Object.keys(xhr()).indexOf("upload") >= 0, "OK!")
 });
 
 test("Detected xhr.upload.onprogress", function(){
-  var req = new XMLHttpRequest();
-  notStrictEqual(req.upload.onprogress, undefined, "OK!")
+  ok(Object.keys(xhr().upload).indexOf("onprogress") >= 0, "OK!")
 });
